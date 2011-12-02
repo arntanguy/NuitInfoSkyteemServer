14e59b8abc831e647ee7bda5805f865db875d5e9
cyril <cyril@cyril-laptop.(none)>
2011-12-02 03:48:37 +0100

    Update Function

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -6,8 +6,8 @@ function afficherAmis($tabamis)	{
 	foreach ($data as $ami)	{
 		// Récupérer l'image de profil
 		?>
-		<li>
-		<img src="https://graph.facebook.com/<?php echo $ami['id']; ?>/picture"> <h3><?php echo $ami['name']; ?></h3>
+		<li style="min-height:50px;padding:0px;padding-left:70px;">
+			<img src="https://graph.facebook.com/<?php echo $ami['id']; ?>/picture"> <h3><?php echo $ami['name']; ?></h3>
 		</li>
 		<?php 
 		
