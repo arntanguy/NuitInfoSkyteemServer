02baceb2dcc7336ea654bc7fefc07ad34de3661c
cyril <cyril@cyril-laptop.(none)>
2011-12-02 03:35:26 +0100

    Update friend and index

--- friends.php
+++ friends.php
@@ -31,24 +31,24 @@
   <head>
     <title>EasyGift - Friends</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
     <link rel="stylesheet" href="style.css" />
-	<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
     <script type="text/javascript" charset="utf-8" src="phonegap.js"></script>      
     <script type="text/javascript" charset="utf-8" src="phoneapp.js"></script>      
 
  </head>
 
  <body onload="onLoad();">
-<div data-role="page">
+<div data-role="page" id="page">
 
     <div data-role="header" id="header">
         <h1>EasyGift - Friends</h1>
-        <img src="images/icones/quit.png" alt="quit" class="quit" onClick="redirect('index.html')">
-        <img src="images/icones/back.png" alt="back" class="back" onClick="redirect('index.html')">
+        <img src="/images/icones/quit.png" alt="quit" class="quit" onClick="redirect('index.html')">
+        <img src="/images/icones/back.png" alt="back" class="back" onClick="redirect('index.html')">
   </div>
-	<div data-role="content">	
+	<div data-role="content" id="content">	
         <div id="offer_to">
             <p>Choisisser un ami</p>
         </div>
--- index.php
+++ index.php
@@ -43,43 +43,28 @@
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
     <link rel="stylesheet" href="style.css" />
 		<script type="text/javascript" charset="utf-8">
-					<?php
-						if(!empty($_GET['state'])) {
-					?>
-							window.location = 'http://easygift.druil.net/friends.php/';
-					<?php
-						}
-					?>
+			<?php
+				if(!empty($_GET['state'])) {
+			?>
+					window.location = 'http://easygift.druil.net/friends.php/';
+			<?php
+				}
+			?>
 		</script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
     <script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
     <script type="text/javascript" charset="utf-8" src="phonegap.js"></script>
 		<script type="text/javascript" charset="utf-8" src="phoneapp.js"></script>
-    <script type="text/javascript" charset="utf-8">
-      // This is your app's init method. Here's an example of how to use it
-      function onLoad() {
-        document.addEventListener("deviceready", onDR, false);
-      }
-
-      function onDR(){
-        // add event listener when the backbutton is pressed
-        document.addEventListener("backbutton", backKeyDown, true);
-      }
-
-      // Handles how the application is deleted:
-      function backKeyDown() {
-        //TODO
-        //alert('go back!');
-        navigator.app.exitApp()
-      }
-    </script>
   </head>
   <body onload="onLoad();">
     <!-- page -->
     <div data-role="page" id="page">
       <!-- header -->
       <div data-role="header" id="header">
-        <h1>EasyGift - <?php echo $user_profile ?></h1>
+        <h1>EasyGift <?php 
+						if($user) {
+							echo(" - ".$user_profile['name']);
+						} ?></h1>
         <img src="images/icones/quit.png" alt="quit" class="quit">
       </div>
 			<?php echo $_SERVER["REQUEST_URI"] ?>
@@ -88,6 +73,7 @@
         <img src="images/logo.png" alt="Logo" class="logo">
         <?php if ($user): ?>
 					<a href="<?php echo $logoutUrl; ?>" data-role="button" data-icon="delete">Logout</a>
+					<a href="friends.php" data-role="button" data-icon="check" >Mes amis</a>
 				<?php else: ?>
 					<a href="<?php echo $loginUrl; ?>" data-role="button" data-icon="check" >Connect with Facebook</a>
 				<?php endif ?>
