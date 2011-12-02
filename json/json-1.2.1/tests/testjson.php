fa2dc4e431fa8ad47d6182abae4892375207c828
TANGUY Arnaud <arn.tanguy@gmail.com>
2011-12-02 01:49:46 +0100

    Fix json file

--- json/json-1.2.1/tests/testjson.php
+++ json/json-1.2.1/tests/testjson.php
@@ -1,7 +1,8 @@
 <?php
-	creatURL("music artiste", "The Police", NULL);
+	creatURL('/music/artist', "The Police", "");
 	
 	function creatURL($type, $name, $album){
+        if(empty($album)) $album = "[]";
 
 		$guill = "%22";
 		$space = "%20";
@@ -13,12 +14,17 @@
 		
 		$url = $url.$urlsuite;
 		echo "\n".$url;
-		$url = urlencode($url);
+        // XXX HACK : fix json_encode return value to make it work
+		$url = str_replace(':"[]"', ':[]', $url);
+        $url = str_replace('"', $guill, $url);
+        $url = str_replace(' ', $space, $url);
+        $url = str_replace('\\', '', $url);
 		
 		echo "\n".$url;
 		getObjJSON($url);
 		
 		$url = 'https://www.googleapis.com/freebase/v1/mqlread?query={%22type%22:%22/music/artist%22,%22name%22:%22The%20Police%22,%22album%22:[]}';
+                https://www.googleapis.com/freebase/v1/mqlread?query={%22type%22:%22/music/artiste%22,%22name%22:%22The%20Police%22,%22album%22:[]}
 	}
 	
 	function getObjJSON($url){
